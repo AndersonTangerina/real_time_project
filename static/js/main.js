a1bcb9ace2429adbcc1f4735baa0bcb791a295a4
Anderson Resende <andersonresende@MBP-de-Anderson.home>
2015-06-07 10:01:17 -0300

    Revert "Changing pusher event and channel"
    This reverts commit c9f2313d28f7c6521823b5f94d99b68c88137535.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
-// Enable pusher callback
+// Enable pusher logging
+Pusher.log = function(message) {
+  if (window.console && window.console.log) {
+    window.console.log(message);
+  }
+};
+
 var pusher = new Pusher('c7bc422dd5eda7e850d1');
-var channel = pusher.subscribe('real_time_channel');
-channel.bind('real_time_event', function(data) {
+var channel = pusher.subscribe('test_channel');
+channel.bind('my_event', function(data) {
   alert(data.message);
 });
