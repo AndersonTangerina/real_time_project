48fa420e05d853006395596338620a648c64147e
Anderson Resende <andersonresende@MBP-de-Anderson.home>
2015-06-07 17:25:07 -0300

    Setup facebook subscriptions without security

--- real_time.py
+++ real_time.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask.ext.triangle import Triangle
+from utils import crossdomain
+from pusher_config import pusher
 
 app = Flask(__name__)
 Triangle(app)
@@ -12,6 +14,19 @@ def index():
 
     return render_template("index.html",)
 
+@app.route('/real', methods=['POST', 'GET'])
+@crossdomain(origin='*')
+def real_time_receiver():
+    """
+    Receive updates from facebook and send to Pusher.
+    """
 
+    if request.method == 'GET':
+        return request.args.get('hub.challenge', 'Not Authorized!')
+    else:
+        object_json = request.json['object']
+        entry_json = request.json['entry']
+        pusher.trigger(u'real_time_channel', u'real_time_event', {u'message': entry_json})
+        return "Facebook Post Update"
 
 
