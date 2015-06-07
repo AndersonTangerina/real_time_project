f90f199024fda1dbb9ba1c729bf57691c584bcdf
Anderson Resende <andersonresende@MBP-de-Anderson.home>
2015-06-07 15:40:15 -0300

    Deleting duplicate face app

--- static/js/main.js
+++ static/js/main.js
@@ -6,22 +6,3 @@ var channel = pusher.subscribe('real_time_channel');
 channel.bind('real_time_event', function(data) {
   alert(data.message);
 });
-
-//----------------------------------------------------------------
-// Facebook App
-//----------------------------------------------------------------
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1603731856535966',
-      xfbml      : true,
-      version    : 'v2.3'
-    });
-  };
-
-(function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
