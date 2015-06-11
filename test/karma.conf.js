02a6909504e945d4399098324f9b603b8cbaf6b3
Anderson Resende <andersonresende@MBP-de-Anderson.home>
2015-06-11 20:18:12 -0300

    Configure karma for unit tests

---
+++ test/karma.conf.js
@@ -0,0 +1,32 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'static/bower_components/angular/angular.js',
+      'static/bower_components/angular-facebook/lin/angular-facebook.js',
+      'static/bower_components/ngmap/build/scripts/ng-map.js',
+      'static/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
